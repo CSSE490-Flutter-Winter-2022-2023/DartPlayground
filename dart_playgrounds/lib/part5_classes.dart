b8f4d2e71f4e69fa07251ffb91dd669d6db22b24
David Fisher <fisherds@rose-hulman.edu>
2022-12-09 13:38:50 -0500

    classes (mostly)

---
+++ dart_playgrounds/lib/part5_classes.dart
@@ -0,0 +1,105 @@
+void main() {
+  print("Classes");
+  // If they are named...
+  // var pt = DoublePoint(
+  //   x: 2,
+  //   y: 5,
+  // );
+
+  // If they are positional...
+  var pt = DoublePoint(2, 5);
+  print("pt = ${pt.x} ${pt.y}  $pt");
+}
+
+// Class DoublePoint
+// var pt = DoublePoint(2, 5);
+
+// Easy solution using the keyword late
+// class DoublePoint {
+//   late double x;
+//   late double y;
+
+//   DoublePoint(double x, double y) {
+//     this.x = 2.0 * x;
+//     this.y = 2.0 * y;
+//   }
+
+//   @override
+//   String toString() {
+//     return "($x, $y)";
+//   }
+// }
+
+// Another solution is to provide a default value.
+// class DoublePoint {
+//   double x = 0;
+//   double y = 0;
+
+//   DoublePoint(double x, double y) {
+//     this.x = 2.0 * x;
+//     this.y = 2.0 * y;
+//   }
+
+//   @override
+//   String toString() {
+//     return "($x, $y)";
+//   }
+// }
+
+// Another solution is to make them nullable.
+// class DoublePoint {
+//   double? x;
+//   double? y;
+
+//   DoublePoint(double x, double y) {
+//     this.x = 2.0 * x;
+//     this.y = 2.0 * y;
+//   }
+
+//   @override
+//   String toString() {
+//     return "($x, $y)";
+//   }
+// }
+
+// Pretty good solution using a new Dart constructor tick!
+// class DoublePoint {
+//   double x;
+//   double y;
+
+//   // Most common, just assign it!
+//   // DoublePoint({required this.x, required this.y});
+//   // DoublePoint(this.x, this.y);
+
+//   // You can also do more
+//   DoublePoint(this.x, this.y) {
+//     x *= 2.0;
+//     y *= 2.0;
+//   }
+
+//   @override
+//   String toString() {
+//     return "($x, $y)";
+//   }
+// }
+
+class DoublePoint {
+  double x;
+  double y;
+
+  // Most common, just assign it!
+  // DoublePoint({required this.x, required this.y});
+  // DoublePoint(this.x, this.y);
+
+  // You can also do more
+  DoublePoint(double x, double y)
+      : this.x = 2.0 * x,
+        this.y = 2.0 * y {
+    print("you made a point");
+  }
+
+  @override
+  String toString() {
+    return "($x, $y)";
+  }
+}
